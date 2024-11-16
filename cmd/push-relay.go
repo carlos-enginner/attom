f017c0ff68976057f2523f1be1d02aa1879d5e47
Carlos Vargas <gyn.carlos@gmail.com>
2024-11-16 11:27:03 -0300

    adjust: set formater type archive

--- cmd/push-relay.go
+++ cmd/push-relay.go
@@ -25,19 +25,19 @@ func Execute() error {
 }
 
 // to do:
-// incluir a coluna do serviço na query que retorna o evento disparado;
+// incluir a coluna do serviço na query que retorna o evento disparado; feito
 // segmentar o código da notificação em um package o NotifyDatabase;
 // sepaar os commands em comandos separados;
 // adicionar o cobra para ter as opções:
 // - adicionar a opção de instalar o notify_database;
 // - adicionar a opção para instalar o software no serviço de start do windows;
 // - adicionar a opção de auto_update para checagem das atualizações;
-// adicionar o viper para uso das variaveis e configuração/leitura do json;
+// adicionar o viper para uso das variaveis e configuração/leitura do json; feito
 // - ajustar a lógica de direcionamento;
 // resolver o erro ao disparar o payload "Error: toml: cannot unmarshal object into Go struct field Association.panels.IdLocalAtendimento of type string" - feito
-// criar models (para centralizar) para o struct do template de config da aplicação;
-// ter um lugar central para escrever e ler o json do template;
-// tem um models do payload da api externa;
+// criar models (para centralizar) para o struct do template de config da aplicação; feito
+// ter um lugar central para escrever e ler o json do template; feito
+// tem um models do payload da api externa; feito
 // criar o build para windows
 // criar o CI/CD no github;
 // centralizar o environment, err := utils.LoadConfig() em uma constante global
