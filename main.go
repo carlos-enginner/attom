5fd534f9df78d3016b3254a643eae49938e7f677
Carlos Vargas <gyn.carlos@gmail.com>
2024-11-16 12:58:40 -0300

    feat: setting version application

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"os"
 	"src/post_relay/cmd"
+	"src/post_relay/config"
 )
 
-var Version, Commit, Date string
+// var Version, Commit, Date string
 
 func main() {
-	fmt.Printf("Version: %s Commit: %s, Date: ", Version, Commit, Date)
+	fmt.Printf("Version: %s Commit: %s, Date: ", config.Version, config.Commit)
 	if err := cmd.Execute(); err != nil {
 		fmt.Println("Error executing command:", err)
 		os.Exit(1)
