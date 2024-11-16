06b4d53ea3ba16dfb357f7290dfd8d2fd62b72b7
Carlos Vargas <gyn.carlos@gmail.com>
2024-11-15 23:27:48 -0300

    chore: adding githooks

---
+++ scripts/install-hooks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Verifica se o diretório .git/hooks existe
+if [ ! -d ".git/hooks" ]; then
+  echo "Diretório .git/hooks não encontrado. Este repositório é um repositório Git?"
+  exit 1
+fi
+
+# Copia todos os hooks do diretório .githooks para .git/hooks
+cp .githooks/* .git/hooks/
+
+# Torna os hooks executáveis
+chmod +x .git/hooks/*
+echo "Hooks instalados com sucesso!"
