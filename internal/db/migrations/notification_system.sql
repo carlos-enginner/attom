6e585a1e87c02cfbedaf2619446f3c1a5e422204
Carlos Vargas <gyn.carlos@gmail.com>
2024-12-03 21:15:48 -0300

    feat: adjusting query

--- internal/db/migrations/notification_system.sql
+++ internal/db/migrations/notification_system.sql
@@ -22,26 +22,15 @@ select
 	UPPER(tc.no_cidadao) as cidadao
 from
 	tb_atend t1
-join tb_atend_prof tap on
-	t1.co_seq_atend = tap.co_atend
-join tb_lotacao tl on
-	tap.co_lotacao = tl.co_ator_papel
-join tb_cbo tc2 on
-	tl.co_cbo = tc2.co_cbo
-join tb_prontuario tp on
-	t1.co_prontuario = tp.co_seq_prontuario
-join tb_cidadao tc on
-	tp.co_cidadao = tc.co_seq_cidadao
-join tb_unidade_saude tus on
-	t1.co_unidade_saude = tus.co_seq_unidade_saude
-join tb_status_atend tsa on
-	t1.st_atend = tsa.co_status_atend
-join tb_prof tp2 on
-	tl.co_prof = tp2.co_seq_prof
-where
-	to_char(t1.dt_criacao_registro ,
-	'dd/mm/yyyy') = to_char(now() ,
-	'dd/mm/yyyy');
+join tb_atend_prof tap on t1.co_seq_atend = tap.co_atend
+join tb_lotacao tl on tap.co_lotacao = tl.co_ator_papel
+join tb_cbo tc2 on tl.co_cbo = tc2.co_cbo
+join tb_prontuario tp on t1.co_prontuario = tp.co_seq_prontuario
+join tb_cidadao tc on tp.co_cidadao = tc.co_seq_cidadao
+join tb_unidade_saude tus on t1.co_unidade_saude = tus.co_seq_unidade_saude
+join tb_status_atend tsa on t1.st_atend = tsa.co_status_atend
+join tb_prof tp2 on tl.co_prof = tp2.co_seq_prof
+where to_char(t1.dt_criacao_registro,'dd/mm/yyyy') = to_char(now(),'dd/mm/yyyy');
 
 
 -- table
